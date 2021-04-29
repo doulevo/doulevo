48bb55d6bd5b2db62da1bbb06085d95766663a64
Ashley Davis <ashley@codecapers.com.au>
2021-04-29 16:25:11 +1000

    Added a test of invoking external code modules from a Nexe build. This might be a good way to implement plugins in Doulevo.

---
+++ plugins/test.js
@@ -0,0 +1,2 @@
+
+console.log("Just a plugin!");
--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
+const minimist = require("minimist");
+const path = require("path");
+
+const argv = minimist(process.argv.slice(2));
 
 console.log("Hello world!");
+
+console.log("Arguments: ");
+
+console.log(argv);
+
+if (argv.plugin) {
+    console.log(`Invoking plugin ${argv.plugin}.`);
+    require(path.join(__dirname, "../plugins", argv.plugin));
+}

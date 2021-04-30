970509ceea84a634077f011c0c9fb36a3bd38630
Ashley Davis <ashley@codecapers.com.au>
2021-04-30 19:15:34 +1000

    Prototyped a structure for implementing commands.

---
+++ src/commands/create.ts
@@ -0,0 +1,4 @@
+
+export default async function (argv: any): Promise<void> {
+    console.log("Create");
+}
--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,29 @@ const path = require("path");
 //
 // process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share")
 
+import create from "./commands/create";
+import up from "./commands/up";
+
 async function main(): Promise<void> {
     const argv = minimist(process.argv.slice(2));
 
-    console.log("Hello world!");
-    
-    console.log("Arguments: ");
-    
-    console.log(argv);
-    
-    if (argv.plugin) {
-        console.log(`Invoking plugin ${argv.plugin}.`);
-        require(path.join(__dirname, "../plugins", argv.plugin));
-    }
-    
     console.log("HOME: " + process.env.HOME);
     console.log("APPDATA: " + process.env.APPDATA);
     console.log("Platform: " + process.platform);
+
+    if (argv._.length > 0) {
+        const cmd = argv._[0];
+        if (cmd === "create") {
+            await create(argv);
+        }
+        else if (cmd === "up") {
+            await up(argv);
+        }
+        else {
+            throw new Error(`Unexpected command ${cmd}`);
+        }
+    }
+
 }
 
 main()

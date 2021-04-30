87ab4b1734193b3f3263e50b2856565978ab2de4
Ashley Davis <ashley@codecapers.com.au>
2021-04-30 19:10:19 +1000

    Moved code into a main function.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,33 @@
 const minimist = require("minimist");
 const path = require("path");
 
-const argv = minimist(process.argv.slice(2));
+//
+// https://stackoverflow.com/a/26227660/25868
+//
+// process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share")
 
-console.log("Hello world!");
+async function main(): Promise<void> {
+    const argv = minimist(process.argv.slice(2));
 
-console.log("Arguments: ");
-
-console.log(argv);
-
-if (argv.plugin) {
-    console.log(`Invoking plugin ${argv.plugin}.`);
-    require(path.join(__dirname, "../plugins", argv.plugin));
+    console.log("Hello world!");
+    
+    console.log("Arguments: ");
+    
+    console.log(argv);
+    
+    if (argv.plugin) {
+        console.log(`Invoking plugin ${argv.plugin}.`);
+        require(path.join(__dirname, "../plugins", argv.plugin));
+    }
+    
+    console.log("HOME: " + process.env.HOME);
+    console.log("APPDATA: " + process.env.APPDATA);
+    console.log("Platform: " + process.platform);
 }
+
+main()
+    .catch(err => {
+        console.error(`Failed:`);
+        console.error(err);
+        process.exit(1);
+    });

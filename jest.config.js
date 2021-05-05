de1ed2c0d280967ce0cb4c22f894b3467177f70d
Ashley Davis <ashley@codecapers.com.au>
2021-05-05 17:15:18 +1000

    Ignoring directories that don't contain tests.

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     modulePathIgnorePatterns: [
-        "<rootDir>/build"
+        "<rootDir>/build",
+        "<rootDir>/test-project",
+        "<rootDir>/node_modules"
     ]
 };

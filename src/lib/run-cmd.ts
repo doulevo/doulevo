356588249a7e44a09701b3a53013d10d81265de5
Ashley Davis <ashley@codecapers.com.au>
2021-05-06 19:11:38 +1000

    Now allowing stand input to be piped into a command, this is required for piping the Dockerfile into the docker command.

--- src/lib/run-cmd.ts
+++ src/lib/run-cmd.ts
@@ -1,11 +1,12 @@
 import { exec } from "child_process";
+import * as stream from "stream";
 
 //
 // Runs a command.
 //
 export function runCmd(cmd: string, options?: any): Promise<void> {
     return new Promise<void>((resolve, reject) => {
-        exec(cmd, options, (error, stdout, stderr) => {
+        const proc = exec(cmd, options, (error, stdout, stderr) => {
             if (error) {
                 //todo: display output?
                 reject(error);
@@ -14,5 +15,15 @@ export function runCmd(cmd: string, options?: any): Promise<void> {
                 resolve();
             }
         });
+
+        if (options.stdin) {
+            //
+            // https://stackoverflow.com/a/41343999/25868
+            //
+            var stdinStream = new stream.Readable();
+            stdinStream.push(options.stdin);    // Add data to the internal queue for users of the stream to consume
+            stdinStream.push(null);             // Signals the end of the stream (EOF)
+            stdinStream.pipe(proc.stdin!);
+        }
     });
 }
